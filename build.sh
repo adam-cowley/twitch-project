d72fa4fdda0995e3e2b95a21e1de07e5f382a58f
Adam Cowley <github@adamcowley.co.uk>
2020-09-29 08:25:14 +0100

    Add npm install to build commants

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build Prod
-cd api && ENV=production npm run build
+cd api && npm i && ENV=production npm run build
 
 # Build UI
-cd ../ui && ENV=production npm run build
+cd ../ui && npm i && ENV=production npm run build
 
 # Copy UI to Server
 cp -R dist/* ../api/public
